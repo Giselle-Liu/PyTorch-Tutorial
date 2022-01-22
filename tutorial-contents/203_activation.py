52b7a8c8fc8fa23b2fafb6d539b3b8aff13af45c
giselle-liu <bluetouchstone17@gmail.com>
2022-01-22 22:33:22 +0800

    vscode Format Document

--- tutorial-contents/203_activation.py
+++ tutorial-contents/203_activation.py
@@ -14,13 +14,13 @@ import matplotlib.pyplot as plt
 # fake data
 x = torch.linspace(-5, 5, 200)  # x data (tensor), shape=(100, 1)
 x = Variable(x)
-x_np = x.data.numpy()   # numpy array for plotting
+x_np = x.data.numpy()  # numpy array for plotting
 
 # following are popular activation functions
 y_relu = torch.relu(x).data.numpy()
 y_sigmoid = torch.sigmoid(x).data.numpy()
 y_tanh = torch.tanh(x).data.numpy()
-y_softplus = F.softplus(x).data.numpy() # there's no softplus in torch
+y_softplus = F.softplus(x).data.numpy()  # there's no softplus in torch
 # y_softmax = torch.softmax(x, dim=0).data.numpy() softmax is a special kind of activation function, it is about probability
 
 # plt to visualize these activation function
